d06f07d40d28c7a0a61ff4eb46714a597f7e9f89
Rough Rigger <riggerrough@protonmail.com>
2021-06-19 21:20:41 +0100

    more dotbot

---
+++ config/fish/config.fish
@@ -0,0 +1,7 @@
+if status --is-login
+	# start sway
+	if test (tty) = '/dev/tty1'
+		exec sway &> /tmp/sway_(date "+%y-%m-%dT%H%M%S")
+		#exec sway &> /tmp/sway_$USER_(date "+%y-%m-%dT%H%M%S")
+	end
+end
---
+++ config/fish/functions/fish_greeting.fish
@@ -0,0 +1,5 @@
+# Defined interactively
+function fish_greeting
+figlet -f standard morning sunshine
+fortune
+end
