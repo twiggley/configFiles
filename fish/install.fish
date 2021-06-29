37d170fe7659cb2f07c2f08329554d8c095a4b2a
Rough Rigger <riggerrough@protonmail.com>
2021-06-29 21:19:48 +0100

    fisher install now conditional

--- fish/install.fish
+++ fish/install.fish
@@ -1,5 +1,11 @@
 #!/usr/bin/env fish
 
+if ! command -v fisher; 
+    echo fisher already installed, upgrading
+    fisher update
+	exit;
+end
+
 # fisher
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 
