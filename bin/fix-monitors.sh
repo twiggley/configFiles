568d4f97c3617145e81366154f205a36e6f15d3b
Twiggley <tomw9003@gmail.com>
2021-11-01 18:21:46 +0000

    updated fix-monitors

--- bin/fix-monitors.sh
+++ bin/fix-monitors.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 #
 # fix startup things since not everything seems to run on i3/polybar load
+#
+# repurposed for general fixing shite, run at startup
 
-xrandr --output DP-1 --right-of DP-3
+# fix layout (bigboi)
+#xrandr --output DP-1 --right-of DP-3
+
+# set primary monitor for x/wayland reasons (chonkyboi)
+xrandr --output XWAYLAND2 --primary
